392ade5a0deb8fc8526142e9a50da85efd4eb649
Jan Havránek <13387356+h4vry@users.noreply.github.com>
2020-02-08 01:23:47 +0100

    Fix headers, reformat code
    Add header Accept which sets the format of returned content from API.
    Without it, some resources returned only plain text which caused the XML
    parser to fail.

--- __init__.py
+++ __init__.py
@@ -7,100 +7,101 @@ import requests_cache
 import re
 from datetime import datetime, date
 
-HEADERS={"content-type": "applicaion/xml;encoding=utf-8",
-         "accept-language": "cs"}
+HEADERS = {"accept": "application/atom+xml",
+           "content-type": "application/xml; encoding=utf-8",
+           "accept-language": "cs"}
 
-requests_cache.install_cache('kosapy_cache', expire_after=24*60*60)
+requests_cache.install_cache('kosapy_cache', expire_after=24 * 60 * 60)
+
 
 class Kosapy:
     def __init__(self, url, auth=None, token=None, verbose=False):
-        self._kosapi=url
-        self._auth=auth
-        self._token=token
-        self._verbose=verbose
-        self._resources={}
+        self._kosapi = url
+        self._auth = auth
+        self._token = token
+        self._verbose = verbose
+        self._resources = {}
 
         assert auth or token
 
     def __getattr__(self, item):
         if item not in self._resources:
-            self._resources[item]=Resource(item, self)
+            self._resources[item] = Resource(item, self)
 
         return self._resources[item]
 
     def get_feed(self, location, params={}):
         if self._verbose:
-            print("Fetching "+self._kosapi+location)
-            
-        if self._token:
-            HEADERS["authorization"] = "Bearer {}".format(self._token)
-            r=requests.get(self._kosapi+location, params=params, headers=HEADERS)
-        else:
-            r=requests.get(self._kosapi+location, auth=self._auth, params=params, headers=HEADERS)
+            print("Fetching " + self._kosapi + location)
 
-        r.encoding='utf-8'
-        if r.status_code!=200:
-            if r.status_code==403:
+        if self._token:
+            HEADERS['Authorization'] = f'Bearer {self._token}'
+            r = requests.get(self._kosapi + location, params=params, headers=HEADERS)
+        else:
+            r = requests.get(self._kosapi + location, auth=self._auth, params=params, headers=HEADERS)
+
+        r.encoding = 'utf-8'
+        if r.status_code != 200:
+            if r.status_code == 403:
                 raise Exception('Wrong authentication')
-            elif r.status_code==404:
-                raise Exception('Resource %s not found'%location)
-            elif r.status_code>=500:
+            elif r.status_code == 404:
+                raise Exception('Resource %s not found' % location)
+            elif r.status_code >= 500:
                 raise Exception('Internal error')
             else:
-                raise Exception('Error %d requesting %s'%(r.status_code, location))
+                raise Exception('Error %d requesting %s' % (r.status_code, location))
 
         return ObjectiveKosapiDoc(bytes(r.text, 'utf-8'), self)
 
     def get_contents(self, feed):
-        feed=(feed.get("atom:feed") if feed.get("atom:feed") else feed).get("atom:entry")
+        feed = (feed.get("atom:feed") if feed.get("atom:feed") else feed).get("atom:entry")
 
         if not feed:
             return ()
 
         for e in feed:
-            c=e.get("atom:content")
-            selff=e.get("atom:link", rel="self")
+            c = e.get("atom:content")
+            selff = e.get("atom:link", rel="self")
             if selff:
-                href=selff("href")
-                if href[-1]=="/":
-                    href=href[:-1]
+                href = selff("href")
+                if href[-1] == "/":
+                    href = href[:-1]
 
-                c.resource=Resource(href, self)
+                c.resource = Resource(href, self)
         return (e.get("atom:content") for e in feed) if feed else ()
 
     def use_cache(self, use):
         if use:
-            requests_cache.install_cache('kosapy_cache', expire_after=24*60*60)
+            requests_cache.install_cache('kosapy_cache', expire_after=24 * 60 * 60)
         else:
             requests_cache.uninstall_cache()
 
 
-
 class Resource:
     def __init__(self, location, api):
-        self._location=location
-        self._params={}
-        self._children={}
-        self._api=api
-        self._content=None
+        self._location = location
+        self._params = {}
+        self._children = {}
+        self._api = api
+        self._content = None
 
     def __call__(self, **kwargs):
         if kwargs:
-            self._params=kwargs
+            self._params = kwargs
             return self
         else:
             if not self._content:
-                self._content=self._api.get_contents(self._api.get_feed(self._location, self._params)).__next__()
+                self._content = self._api.get_contents(self._api.get_feed(self._location, self._params)).__next__()
             return self._content
 
     def __iter__(self):
-        feed=self._api.get_feed(self._location, self._params)
+        feed = self._api.get_feed(self._location, self._params)
         while True:
             for entry in self._api.get_contents(feed):
                 yield entry
 
             if feed.get("atom:feed") and feed.get("atom:feed").get("atom:link", rel="next"):
-                feed=self._api.get_feed(feed.get("atom:feed").get("atom:link", rel="next")("href"))
+                feed = self._api.get_feed(feed.get("atom:feed").get("atom:link", rel="next")("href"))
             else:
                 break
 
@@ -109,18 +110,18 @@ class Resource:
 
     def __getattr__(self, item):
         if item not in self._children:
-            self._children[item]=Resource(self._location+"/"+item, self._api)
+            self._children[item] = Resource(self._location + "/" + item, self._api)
 
         return self._children[item]
 
     def __repr__(self):
-        return "<KosAPI resource "+self._location+">"
+        return "<KosAPI resource " + self._location + ">"
 
 
 class ObjectiveKosapiDoc:
     def __init__(self, doc, api):
-        self._api=api
-        self._root=KosapiElement("root", (), api)
+        self._api = api
+        self._root = KosapiElement("root", (), api)
         if doc:
             self._parse_doc(doc)
 
@@ -138,25 +139,25 @@ class ObjectiveKosapiDoc:
 
 
 class KosapiElement:
-    _redatetime=re.compile("^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
-    _redate=re.compile("^\d{4}-\d{2}-\d{2}$")
-    _rebool=re.compile("^true$|^false$")
-    _redigit=re.compile("^\d+$")
+    _redatetime = re.compile("^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$")
+    _redate = re.compile("^\d{4}-\d{2}-\d{2}$")
+    _rebool = re.compile("^true$|^false$")
+    _redigit = re.compile("^\d+$")
 
     def __init__(self, name, attrs, api):
-        self._content=""
-        self._content_parsed=""
-        self._name=name
-        self._attrs=attrs
+        self._content = ""
+        self._content_parsed = ""
+        self._name = name
+        self._attrs = attrs
 
-        self._children={}
-        self._ref=None
-        self._api=api
+        self._children = {}
+        self._ref = None
+        self._api = api
 
     def __getattr__(self, item):
         if "xlink:href" in self._attrs:
             if not self._ref:
-                self._ref=Resource(self._attrs.getValue("xlink:href"), self._api)
+                self._ref = Resource(self._attrs.getValue("xlink:href"), self._api)
             return self._ref().get(item)
 
         if item in self._children:
@@ -181,34 +182,34 @@ class KosapiElement:
         if kwargs and item in self._children:
             for el in self._children[item]:
                 for attr, value in kwargs.items():
-                    if el(attr)==value:
+                    if el(attr) == value:
                         return el
         else:
             return self.__getattr__(item)
 
     def __repr__(self):
-        return "<KosAPI entry "+self._name+">"
+        return "<KosAPI entry " + self._name + ">"
 
     def _parse_content(self):
         if self._redigit.match(self._content):
-            self._content_parsed=int(self._content)
+            self._content_parsed = int(self._content)
         elif self._redatetime.match(self._content):
-            self._content_parsed=datetime.strptime(self._content, "%Y-%m-%dT%H:%M:%S")
+            self._content_parsed = datetime.strptime(self._content, "%Y-%m-%dT%H:%M:%S")
         elif self._redate.match(self._content):
-            self._content_parsed=datetime.strptime(self._content, "%Y-%m-%d").date()
+            self._content_parsed = datetime.strptime(self._content, "%Y-%m-%d").date()
         elif self._rebool.match(self._content):
-            self._content_parsed=self._content.lower()=="true"
+            self._content_parsed = self._content.lower() == "true"
         else:
-            self._content_parsed=self._content
+            self._content_parsed = self._content
 
     def add_element(self, element):
         if element._name in self._children:
             if isinstance(self._children[element._name], list):
                 self._children[element._name].append(element)
             else:
-                self._children[element._name]=[self._children[element._name], element]
+                self._children[element._name] = [self._children[element._name], element]
         else:
-            self._children[element._name]=element
+            self._children[element._name] = element
 
     def traverse(self):
         if self._children:
@@ -217,16 +218,16 @@ class KosapiElement:
                 for sub in child:
                     sub.traverse()
         else:
-            print("%s: %s (%s)"%(self._name, self._content, str(self._attrs.getNames())))
+            print("%s: %s (%s)" % (self._name, self._content, str(self._attrs.getNames())))
 
 
 class KosapiSaxHandler(xml.sax.ContentHandler):
     def __init__(self, root, api):
-        self.path=[root]
-        self._api=api
+        self.path = [root]
+        self._api = api
 
     def startElement(self, name, attrs):
-        element=KosapiElement(name, attrs, self._api)
+        element = KosapiElement(name, attrs, self._api)
         self.path[-1].add_element(element)
         self.path.append(element)
 
@@ -234,4 +235,4 @@ class KosapiSaxHandler(xml.sax.ContentHandler):
         self.path.pop()
 
     def characters(self, content):
-        self.path[-1]._content+=content.strip()
+        self.path[-1]._content += content.strip()
